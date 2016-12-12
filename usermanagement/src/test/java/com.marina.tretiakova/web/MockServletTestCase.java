b8d4b543dd29b357074d7064061d8bcca299c03f
Marina Tretiakova <maryna.tretiakova@nure.ua>
2016-12-13 01:54:52 +0200

    Laba 4 : Web-app

---
+++ usermanagement/src/test/java/com.marina.tretiakova/web/MockServletTestCase.java
@@ -0,0 +1,38 @@
+package com.marina.tretiakova.web;
+
+import java.util.Properties;
+
+import com.mockobjects.dynamic.Mock;
+import com.mockrunner.servlet.BasicServletTestCaseAdapter;
+
+import com.marina.tretiakova.db.DaoFactory;
+import com.marina.tretiakova.db.MockDaoFactory;
+
+public abstract class MockServletTestCase extends BasicServletTestCaseAdapter {
+
+	private Mock mockUserDao;
+
+	public Mock getMockUserDao() {
+		return mockUserDao;
+	}
+
+	public void setMockUserDao(Mock mockUserDao) {
+		this.mockUserDao = mockUserDao;
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		Properties properties = new Properties();
+		properties.setProperty("dao.factory", MockDaoFactory.class.getName());
+		DaoFactory.init(properties);
+		setMockUserDao((((MockDaoFactory) DaoFactory.getInstance()).getMockUserDao()));
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		getMockUserDao().verify();
+		super.tearDown();
+	}
+
+}
